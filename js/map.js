b13c71aa9a38543259a62148866b1ec9088c3236
gerlinda137 <gerlinda137@gmail.com>
2020-03-01 18:38:26 +0300

    moved renderPin to a separate module

--- js/map.js
+++ js/map.js
@@ -1,30 +1,13 @@
 'use strict';
 
 (function () {
-  var PinSize = {
-    HEIGHT: 70,
-    RADIUS: 50 / 2
-  };
 
   var mapPinsContainer = document.querySelector('.map__pins');
-  var pinTemplate = document
-    .querySelector('#pin')
-    .content.querySelector('.map__pin');
-
-  var renderPin = function (advert) {
-    var pin = pinTemplate.cloneNode(true);
-    var avatar = pin.querySelector('img');
-    pin.style.left = advert.location.x - PinSize.RADIUS + 'px';
-    pin.style.top = advert.location.y - PinSize.HEIGHT + 'px';
-    avatar.src = advert.author.avatar;
-    avatar.alt = advert.offer.title;
-    return pin;
-  };
 
   var addPins = function (adverts) {
     var fragment = document.createDocumentFragment();
     for (var i = 0; i < adverts.length; i++) {
-      var renderedAd = renderPin(adverts[i]);
+      var renderedAd = window.renderPin(adverts[i]);
       fragment.appendChild(renderedAd);
     }
     mapPinsContainer.appendChild(fragment);
---
+++ js/pin.js
@@ -0,0 +1,25 @@
+'use strict';
+
+(function () {
+  var PinSize = {
+    HEIGHT: 70,
+    RADIUS: 50 / 2
+  };
+
+  var pinTemplate = document
+    .querySelector('#pin')
+    .content.querySelector('.map__pin');
+  window.renderPin = function (advert) {
+    var pin = pinTemplate.cloneNode(true);
+    var avatar = pin.querySelector('img');
+
+    pin.style.left = advert.location.x - PinSize.RADIUS + 'px';
+    pin.style.top = advert.location.y - PinSize.HEIGHT + 'px';
+    avatar.src = advert.author.avatar;
+    avatar.alt = advert.offer.title;
+
+    return pin;
+  };
+
+
+})();
