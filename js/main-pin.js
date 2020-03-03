eeda2c411737f6b5b4af51911edb3151162038e4
gerlinda137 <gerlinda137@gmail.com>
2020-03-04 01:39:18 +0300

    dragPin

--- js/main-pin.js
+++ js/main-pin.js
@@ -45,8 +45,55 @@
     }
   };
 
-  var onMainPinMouseDown = function () {
+  var onMainPinMouseDown = function (evt) {
     handleChange(getMainPinCoords(MainPinSize.HEIGHT));
+
+    evt.preventDefault();
+    var startCoordinates = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      dragged = true;
+
+      var shift = {
+        x: startCoordinates.x - moveEvt.clientX,
+        y: startCoordinates.y - moveEvt.clientY
+      };
+
+      startCoordinates = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      mainPin.style.top =
+      mainPin.offsetTop - shift.y + 'px';
+      mainPin.style.left =
+      mainPin.offsetLeft - shift.x + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          mainPin.removeEventListener('click', onClickPreventDefault);
+        };
+        mainPin.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   };
 
   var onMainPinFirstKeyDown = function (evt) {
