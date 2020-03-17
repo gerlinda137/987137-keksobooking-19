9db0a8b5c3174a1db039adcb2c77615eea0c2417
gerlinda137 <gerlinda137@gmail.com>
2020-03-17 22:41:19 +0300

    получила ссылку сразу на элемент, а не на весь шаблон

--- js/splash.js
+++ js/splash.js
@@ -2,11 +2,10 @@
 
 (function () {
   var showMessage = function (elementId, elementClass, exitButtonSelector) {
-    var messageTemplate = document.querySelector(elementId).content;
-    var message = messageTemplate.cloneNode(true);
-    document.body.appendChild(message);
+    var message = document.querySelector(elementId).content.querySelector(elementClass);
+    var messageDiv = message.cloneNode(true);
+    document.body.appendChild(messageDiv);
 
-    var messageDiv = document.querySelector(elementClass);
     var onMessagePress = function (evt) {
       if (window.util.isMainMouseButton(evt) || window.util.isEscKey(evt)) {
         messageDiv.remove();
@@ -22,47 +21,7 @@
     }
   };
 
-  // var showSuccess = function () {
-
-  //   var successMessageTemplate = document.querySelector('#success').content;
-  //   var successMessage = successMessageTemplate.cloneNode(true);
-  //   document.body.appendChild(successMessage);
-
-  //   var successMessageDiv = document.querySelector('.success');
-  //   var onSuccessMessagePress = function (evt1) {
-  //     if (window.util.isMainMouseButton(evt1) || window.util.isEscKey(evt1)) {
-  //       successMessageDiv.remove();
-  //       document.body.removeEventListener('keydown', onSuccessMessagePress);
-  //     }
-  //   };
-
-  //   successMessageDiv.addEventListener('click', onSuccessMessagePress);
-  //   document.body.addEventListener('keydown', onSuccessMessagePress);
-  // };
-
-  // var showError = function () {
-  //   var errorMessageTemplate = document.querySelector('#error').content;
-  //   var errorMessage = errorMessageTemplate.cloneNode(true);
-  //   document.body.appendChild(errorMessage);
-
-  //   var errorMessageDiv = document.querySelector('.error');
-  //   var onErrorMessagePress = function (evt1) {
-  //     if (window.util.isMainMouseButton(evt1) || window.util.isEscKey(evt1)) {
-  //       errorMessageDiv.remove();
-  //       document.body.removeEventListener('keydown', onErrorMessagePress);
-  //     }
-  //   };
-
-  //   var errorButton = errorMessageDiv.querySelector('.error__button');
-
-  //   errorMessageDiv.addEventListener('click', onErrorMessagePress);
-  //   errorButton.addEventListener('click', onErrorMessagePress);
-  //   document.body.addEventListener('keydown', onErrorMessagePress);
-  // };
-
   window.splash = {
-    // showSuccess: showSuccess,
-    // showError: showError,
     showMessage: showMessage,
   };
 })();
